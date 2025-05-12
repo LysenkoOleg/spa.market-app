6f7a798495e236648b96916799245054a723cb90
LysenkoOleg <89065167+LysenkoOleg@users.noreply.github.com>
2025-05-12 23:03:31 +0300

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Main from './pages/Main/Main';
+
+const App = () => {
+  return (
+    <div className="app-window-container">
+	    <div className='app-window'>
+		   <Main />
+	    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Main.css'
+
+class Main extends React.Component {
+	render() {
+		return (
+			<div className='main-container'>
+				<div>menu</div>
+				<div>
+					<div>header</div>
+					<div>content</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Main;
